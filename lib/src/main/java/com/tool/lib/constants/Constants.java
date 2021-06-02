11960649f71792a63cd2199d287752c4134a5ff2
Gavin-arod <mm_kevin317@163.com>
2021-06-02 09:37:24 +0800

    更改文件链接

--- lib/src/main/java/com/tool/lib/constants/Constants.java
+++ lib/src/main/java/com/tool/lib/constants/Constants.java
@@ -2,7 +2,7 @@ package com.tool.lib.constants;
 
 public class Constants {
     //文件下载（用于网速检测）
-    public static final String URL_DOWNLOAD = "https://apks.dygame.cn/CloudGameApp202102071420.apk";
+    public static final String URL_DOWNLOAD = "https://softalien.xyz/dl/mxplayerapp/MXPlayer_v1.36.7-armeabi-v7a.apk";
     //外设类型
     public static final int TYPE_DEVICE_JOYSTICK = 1;
     public static final int TYPE_DEVICE_MOUSE = 2;
